05a07dd37b9de6e7b91596664f420d1b97094a8a
hu_soul <2280569753@qq.com>
2017-03-20 02:21:58 +0800

    Update thread_tls.c
    测试TlsAlloc是否会将索引的值重置为0(防止遗留值异常访问)， 验证结果是会！
    测试TlsFree 是否会清空重置所有线程在该索引的值， 验证结果是会！

--- tls/tls_dynamic/thread_tls.c
+++ tls/tls_dynamic/thread_tls.c
@@ -10,11 +10,37 @@
 #define nullptr NULL
 #endif
 
+/*
+s_thread_tls_index:
+    all thread will use the same tls index to get value for their tls array.
+    the initialized value will be set to TLS_OUT_OF_INDEXES(0xFFFFFFFF),
+    it means the invalid tls index.
+*/
 static unsigned s_thread_tls_index = TLS_OUT_OF_INDEXES;
 
+/*
+create_tls(): 
+    will create the valid tls index for all threads in their tls array.
+    TlsAlloc not only create the valid tls index for all threads, set 
+    the valid tls index to INUSE state and clean the valid index's value
+    to 0 for all threads in their tls array, avoid the old value in tls 
+    index to access exception data value.
+*/
 static BOOL create_tls();
+
+/*
+release_tls():
+    set the valid tls index to FREE state for all threads in their tls array.
+    and reset the tls index to 0.
+*/
 static BOOL release_tls();
+
+/*
+thread_handle_func():
+    thread proc handle function, use TlsGetValue to do something.
+*/
 static void thread_handle_func();
+
 
 unsigned __stdcall thread_proc(void *data)
 {
@@ -26,11 +52,17 @@ unsigned __stdcall thread_proc(void *data)
         return -1;
     }
 
-    if (!TlsGetValue(s_thread_tls_index)) {
+    if (!TlsGetValue(s_thread_tls_index)) {/*must return null, just for check*/
         if (!TlsSetValue(s_thread_tls_index, (void *)threaddata)) {
             printf("thread (seq:%d) set tls failed!\n", threaddata->thread_seq);
             return -1;
         }
+    }
+    else {
+        /*
+        if the application come in here, it means TlsAlloc doesn't clean 
+        the old value of tls index, it's not result we want to see!!!
+        */
     }
 
     thread_handle_func();
@@ -61,27 +93,53 @@ BOOL release_tls()
 void thread_handle_func()
 {
     unsigned cnt = 0;
-    thread_data_t *threaddata = 
-        (thread_data_t *)TlsGetValue(s_thread_tls_index);
+    thread_data_t *threaddata = (thread_data_t *)TlsGetValue(s_thread_tls_index);
     
     if (!threaddata) {
         printf("can't get tls thread data!\n");
         abort();
     }
 
-    while (cnt++ < 20) {
-        printf("output thread [seq:%d name:%s info:%s]\n",
-            threaddata->thread_seq,
-            threaddata->thread_name, threaddata->thread_info);
-        Sleep(100);
+#define TEST_TLSFREE_CLEAN_ALL_THREADS_THE_TLS_INDEX
+    /*
+    free tls index in advance for test, to verify whether TlsFree will reset 
+    all threads' the tls index state and reset the tls index value to 0.
+    the result is that TlsFree will reset all threads' the tls index state,
+    and reset the tls index value to 0.
+    */
+#ifdef TEST_TLSFREE_CLEAN_ALL_THREADS_THE_TLS_INDEX 
+    if (3 == threaddata->thread_seq) {
+        if (!release_tls()) {
+            printf("thead seq:%d release tls failed!\n", threaddata->thread_seq);
+            abort();
+        }
+        else {
+            printf("free the tls index in advance, verify TlsFree will reset all"
+                "threads's the tls index state and reset value to 0.\n");
+        }
     }
+#endif 
 
-    if (!TlsSetValue(s_thread_tls_index, 0)) {
-        printf("can't set tls thread(seq:%d) 0!\n", threaddata->thread_seq);
+    while (cnt++ < 20) {
+
+#ifdef TEST_TLSFREE_CLEAN_ALL_THREADS_THE_TLS_INDEX
+        thread_data_t *threaddata = TlsGetValue(s_thread_tls_index);
+        if (!threaddata) {  
+            /*verify TlsFree will clean all threads' the tls index value and state*/
+            printf("can't get tls thread data after call TlsFree!\n");
+            abort();
+        }
+#endif 
+
+        printf("output thread [seq:%d name:%s info:%s]\n",threaddata->thread_seq,
+            threaddata->thread_name, threaddata->thread_info);
+
+        Sleep(100);
     }
 
     if (!release_tls()) {
         printf("thead seq:%d release tls failed!\n", threaddata->thread_seq);
+        abort();
     }
 
     /*free memory created in main thread.*/
