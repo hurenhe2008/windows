1cb35b90d0fd3f5c2bf07bad2adcae7403229df6
hu_soul <2280569753@qq.com>
2017-03-23 16:49:05 +0800

    测试单元增加单实例、单实例模版简易测试
    测试单元增加单实例、单实例模版简易测试

--- singleton/unit_test.c
+++ singleton/unit_test.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "singleton.h"
+#include "singleton_tmpl.h"
+#include "singleton_app.h"
 
 #define APP_NAME "test_singleton"
 
+
+class TestClass1
+{
+    /* nothing, just test */
+};
+
+class TestClass2
+{
+    /* nothing, just test */
+};
+
 int main(int argc, char *argv[])
 {
-    /*at first, check the app whether already run*/
+    /* whether app already run */
     if (app_exist_ex(APP_NAME)) {
         printf("%s application has already run, exit!\n", APP_NAME);
         exit(0);
     }
 
-    /*
-    ... application real code ...
-    */
+    /* common singleton use */
+    Singleton &test = Singleton::instance();
+
+    /* some front endpoint use */
+    SingletonF &ftest = SingletonF::instance();
+    if (!ftest.init()) {
+        printf("something error when init SingletonF!\n");
+    }
+    
+    /* use template to accomplis singleton effect */
+    TestClass1 &tmpltest1 = SingletonTmpl<TestClass1>::instance();
+    TestClass2 &tmpltest2 = SingletonTmpl<TestClass2>::instance();
 
     return 0;
 }
