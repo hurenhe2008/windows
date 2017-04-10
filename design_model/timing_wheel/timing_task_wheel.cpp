794325ed5361c2c844c36dfd0333935c2c5a0b33
hu_soul <2280569753@qq.com>
2017-04-11 00:59:54 +0800

    Update timing_task_wheel.cpp

--- design_model/timing_wheel/timing_task_wheel.cpp
+++ design_model/timing_wheel/timing_task_wheel.cpp
@@ -8,15 +8,19 @@ TimingTaskWheel* TimingTaskWheel::mp_self = nullptr;
 TimingTaskWheel& TimingTaskWheel::instance()
 {
     if (!mp_self) {
+        /* lock */
         m_inst_mutex.lock();
+        /* double check */
         if (!mp_self) {
+            /* avoid cpu change constructor and assign sequence */
             TimingTaskWheel *tmp = new TimingTaskWheel();
             if (!tmp || !tmp->init()) {
                 delete tmp;
                 tmp = nullptr;
-            }
+            }      
             mp_self = tmp;
         }
+        /* unlock */
         m_inst_mutex.unlock();
     }
 
@@ -57,16 +61,22 @@ bool TimingTaskWheel::init()
 
 bool TimingTaskWheel::uninit()
 {
+    /* stop thread if run */
+    if (!this->stop())  return false;
+
     MutexLocker lock(&m_task_mutex);
 
     for (int i = 0; i < TASK_QUEUE_SIZE; ++i) {
-        task_queue_citr cit = m_task_queue[i].begin();
+
+        task_queue_t::const_iterator cit = m_task_queue[i].begin();
+
         for (; cit != m_task_queue[i].end(); ++cit) {
             /*task not finish, callback notify*/
             if (cit->touch_after_seconds > 0) {
-                cit->nofinish_func(cit->data);
+                cit->cancel_func(cit->data);
             }
         }
+
         m_task_queue[i].clear();
     }
 
@@ -75,25 +85,75 @@ bool TimingTaskWheel::uninit()
 
 
 unsigned TimingTaskWheel::run()
-{
-    const struct timeval time_val = { 1, 0 };
-   
-    /* time interval */
-    int ret = select(0, nullptr, nullptr, nullptr, &time_val);
+{ 
+    struct timeval time_val = { 0 };
+    unsigned last_time = 0;
+    int interval = 0;
+    bool bfirst = true;
+    int retval = 1;
 
-    /* error */
-    if (ret < 0) {  
-        return 0;
+    while (m_brun) {
+        
+        if (!bfirst) {
+            interval = last_time + PERIOD_TIME - GetTickCount();
+            if (interval < 0) {
+                interval = 0;
+            }
+        }
+        else {
+            last_time = GetTickCount();
+            interval = PERIOD_TIME;
+            bfirst = false;
+        }
+
+        time_val.tv_sec = (interval < 1000) ? 0 : (interval / 1000);
+        time_val.tv_usec = (0 == interval) ? 0 : (interval % 1000);
+
+        /* time interval */
+        int ret = select(0, nullptr, nullptr, nullptr, &time_val);
+        if (ret < 0) {
+            if (WSAEINTR == WSAGetLastError()) {
+                continue;
+            }
+            retval = 0;
+            break;
+        }
+
+        handleTaskQueue();
     }
 
-    /* handle task in queue */
-    handleTaskQueue(); 
-
-    return 1;
+    return retval;
 }
 
 void TimingTaskWheel::handleTaskQueue()
 {
+    MutexLocker lock(&m_task_mutex);
+
+    if (m_curr_pos >= TASK_QUEUE_SIZE) {
+        /* error happen */
+        abort(); 
+    }
+ 
+    task_queue_t &queue = m_task_queue[m_curr_pos];
+    task_queue_t::iterator it = queue.begin();
+
+    for (; it != queue.end(); ) {
+        if (it->touch_after_cycles <= 0) {
+            if (!it->touch_func(it->data)) {
+                it->error_func(it->data);
+            }
+            queue.erase(it++);
+        }
+        else {
+            it->touch_after_cycles -= 1;
+            it++;
+        }
+    }
+
+    if (m_curr_pos + 1 < TASK_QUEUE_SIZE)
+        ++m_curr_pos;
+    else
+        m_curr_pos = 0;
 
 }
 
@@ -102,10 +162,23 @@ bool TimingTaskWheel::insertTask(timing_task_t *task)
     MutexLocker lock(&m_task_mutex);
 
     if (m_curr_pos >= TASK_QUEUE_SIZE) {
-        m_curr_pos = 0;
+        abort();
     }
 
-    m_task_queue[m_curr_pos].insert(*task);
+    /* cycles to recycle */
+    unsigned cycles = task->touch_after_seconds / TASK_QUEUE_SIZE;
+    /* insert position */
+    unsigned insert_pos = m_curr_pos + 
+        task->touch_after_seconds % TASK_QUEUE_SIZE;
+    
+    task->touch_after_cycles = cycles;
+
+    if (insert_pos >= TASK_QUEUE_SIZE) {
+        insert_pos = insert_pos % TASK_QUEUE_SIZE;
+    }
+  
+    m_task_queue[insert_pos].insert(*task);
+
     return true;
 }
 
