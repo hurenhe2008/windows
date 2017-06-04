3bc4713566b71136fc53bf92582a9d76f0ff3a9e
hu_soul <2280569753@qq.com>
2017-06-04 14:47:53 +0800

    增加编译检错

--- mutex/mutex_locker.h
+++ mutex/mutex_locker.h
@@ -17,5 +17,8 @@ private:
     Mutex& m_mutex;
 };
 
+//avoid code like:  "MutexLocker(&mutex);"  
+//correct code is:  "MutexLocker locker(&mutex);"
+#define MutexLocker(x)  static_assert(false, "missing MutexLocker variable name")
 
 #endif //_MUTEX_LOCKER_H_
