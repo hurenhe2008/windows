c30e5735c5e2d7c9ac89bb1d80c57c336ab48b18
hu_soul <2280569753@qq.com>
2017-06-04 14:52:23 +0800

    增加编译检错

--- design_model/timing_wheel/mutex_locker.h
+++ design_model/timing_wheel/mutex_locker.h
@@ -17,6 +17,9 @@ private:
     Mutex* mp_mutex;
 };
 
+//avoid code like:  "MutexLocker(&mutex);"  
+//correct code is:  "MutexLocker locker(&mutex);"
+#define MutexLocker(x)  static_assert(false, "missing MutexLocker variable name")
 
 #endif //_MUTEX_LOCKER_H_
 
